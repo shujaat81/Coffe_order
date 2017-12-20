61a8ea28713b5630a0c2e306cca45401cc02c588
shujaat81 <hasanshujaat4@gmail.com>
2017-12-20 16:07:38 +0530

    Making an App Interactive using Java code commit 1

--- app/src/main/java/com/example/hasan/coffe_order/MainActivity.java
+++ app/src/main/java/com/example/hasan/coffe_order/MainActivity.java
@@ -1,13 +1,80 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name. 
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
 package com.example.hasan.coffe_order;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    int valueIncrement=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int priceForOne = 30;
+
+        displayPrice(priceForOne * valueIncrement);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.noforders);
+        quantityTextView.setText(""+number);
+    }
+
+    // Increment method for adding number of coffees
+
+    public void increment(View view){
+
+        valueIncrement+=1;
+        display(valueIncrement);
+
+    }
+
+    // Decrement method for adding number of coffees
+
+    public void decrement(View view){
+
+        if (valueIncrement>0){
+        valueIncrement-=1;
+        display(valueIncrement);
+        }else{
+            display(0);
+        }
+
+    }
+
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
